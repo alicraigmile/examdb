c061de415619969b3814eeedd597b52e61776ec4
Ali Craigmile <alic@galah.home>
2018-06-03 09:00:48 +0100

    node-express-ness

---
+++ server.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const package = require('./package'),
+      morgan = require('morgan'),
+      port = process.env.PORT || 5000,
+      app = require('./lib/app');
+
+var logger,
+    server;
+
+logger = morgan('combined');
+app.use(logger);
+server = app.listen(port, () => console.log(package.name + ' listening on port ' + port +  '!'));
+server.listen(port);
