62dc08720b1ccb204a9869340f11ad6f6bdb3bc8
Ali Craigmile <ali.craigmile@bbc.co.uk>
2018-07-03 07:22:51 +0100

    test(improved testing of search)

--- test/search.js
+++ test/search.js
@@ -18,5 +18,65 @@ describe('Search', function() {
 			});
 	});
 
+	it('Search by Exam board', function(done) {
+		request(app)
+			.get('/search/?q=AQA')
+			.set('Accept', 'text/html')
+			.expect(200)
+			.end(function(err, res) {
+				if (err) return done(err);
+				expect(res.body).to.not.equal(null);
+				done();
+			});
+	});
+
+	it('Search by Qualification', function(done) {
+		request(app)
+			.get('/search/?q=GCSE')
+			.set('Accept', 'text/html')
+			.expect(200)
+			.end(function(err, res) {
+				if (err) return done(err);
+				expect(res.body).to.not.equal(null);
+				done();
+			});
+	});
+
+	it('Search by Date', function(done) {
+		request(app)
+			.get('/search/?q=2018-05-23')
+			.set('Accept', 'text/html')
+			.expect(200)
+			.end(function(err, res) {
+				if (err) return done(err);
+				expect(res.body).to.not.equal(null);
+				done();
+			});
+	});
+
+	it('Search by Date (today)', function(done) {
+		request(app)
+			.get('/search/?q=today')
+			.set('Accept', 'text/html')
+			.expect(200)
+			.end(function(err, res) {
+				if (err) return done(err);
+				expect(res.body).to.not.equal(null);
+				done();
+			});
+	});
+
+	it('Search by Date (tomorrow)', function(done) {
+		request(app)
+			.get('/search/?q=tomorrow')
+			.set('Accept', 'text/html')
+			.expect(200)
+			.end(function(err, res) {
+				if (err) return done(err);
+				expect(res.body).to.not.equal(null);
+				done();
+			});
+	});
+
 });
 
