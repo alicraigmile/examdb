050c8d336ba92d59f28ec9c32750a00f6071b3b7
Ali Craigmile <ali.craigmile@bbc.co.uk>
2018-07-12 01:54:27 +0100

    add eslint checking and comply with said standards (#2)

--- test/qualification.js
+++ test/qualification.js
@@ -1,64 +1,60 @@
-'use strict';
+import { request } from 'supertest';
+import { describe, it, xit } from 'mocha';
+import { expect } from 'chai';
+import app from '../lib/app';
 
-var request = require('supertest'),
-	app = require('../lib/app'),
-	chai = require('chai'),
-		expect = chai.expect;
+describe('Qualification', () => {
+    it('List qualifications in JSON', done => {
+        request(app)
+            .get('/qualifications.json')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body).to.not.equal(null);
+                return done();
+            });
+    });
 
-describe('Qualification', function() {
-	it('List qualifications in JSON', function(done) {
-		request(app)
-			.get('/qualifications.json')
-			.set('Accept', 'application/json')
-			.expect(200)
-			.end(function(err, res) {
-				if (err) return done(err);
-				expect(res.body).to.not.equal(null);
-				done();
-			});
-	});
-	
-	xit('List qualifications in HTML', function(done) {
-		request(app)
-			.get('/qualifications/')
-			.set('Accept', 'text/html')
-			.expect(200)
-			.end(function(err, res) {
-				if (err) return done(err);
-				expect(res.body).to.not.equal(null);
-				done();
-			});
-	});
+    xit('List qualifications in HTML', done => {
+        request(app)
+            .get('/qualifications/')
+            .set('Accept', 'text/html')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body).to.not.equal(null);
+                return done();
+            });
+    });
 
-	xit('Providers by qualification JSON', function(done) {
-		request(app)
-			.get('/qualifications/gcse.json')
-			.set('Accept', 'application/json')
-			.expect(200)
-			.end(function(err, res) {
-				if (err) return done(err);
-				expect(res.body).to.have.property('qualification');
-				expect(res.body.programmeOfStudy).to.have.property('id');
-				expect(res.body.programmeOfStudy.id).to.not.equal(null);
-				expect(res.body.programmeOfStudy).to.have.property('name');
-				expect(res.body.programmeOfStudy.name).to.not.equal(null);
-				expect(res.body).to.have.property('providers');
-				expect(res.body.exams).to.not.equal(null);
-				done();
-			});
-	});
+    xit('Providers by qualification JSON', done => {
+        request(app)
+            .get('/qualifications/gcse.json')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body).to.have.property('qualification');
+                expect(res.body.programmeOfStudy).to.have.property('id');
+                expect(res.body.programmeOfStudy.id).to.not.equal(null);
+                expect(res.body.programmeOfStudy).to.have.property('name');
+                expect(res.body.programmeOfStudy.name).to.not.equal(null);
+                expect(res.body).to.have.property('providers');
+                expect(res.body.exams).to.not.equal(null);
+                return done();
+            });
+    });
 
-	it('Providers by qualification in HTML', function(done) {
-		request(app)
-			.get('/qualifications/gcse')
-			.set('Accept', 'text/html')
-			.expect(200)
-			.end(function(err, res) {
-				if (err) return done(err);
-				expect(res.body).to.not.equal(null);
-				done();
-			});
-	});
-
+    it('Providers by qualification in HTML', done => {
+        request(app)
+            .get('/qualifications/gcse')
+            .set('Accept', 'text/html')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body).to.not.equal(null);
+                return done();
+            });
+    });
 });
-
--- test/status.js
+++ test/status.js
@@ -1,37 +1,26 @@
-'use strict';
+import { request } from 'supertest';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import app from '../lib/app';
 
-var request = require('supertest'),
-	app = require('../lib/app'),
-	chai = require('chai'),
-		expect = chai.expect;
+describe('Status', () => {
+    it('displays the app name', done => {
+        request(app)
+            .get('/status')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(res => expect(res.body.app, 'body.app').to.exist)
+            .end(err => (err ? done(err) : done()));
+    });
 
-describe('Status', function() {
-	it('displays the app name', function(done) {
-		request(app)
-			.get('/status')
-			.set('Accept', 'application/json')
-			.expect('Content-Type', /json/)
-			.expect(200)
-				.expect(function(res) {
-					expect(res.body.app, 'body.app').to.exist;
-				})
-				.end(function(err, res) {
-					if (err) return done(err);
-					done();
-				});
-		});
-
-	it('displays the app version number', function(done) {
-		request(app)
-			.get('/status')
-			.set('Accept', 'application/json')
-			.expect('Content-Type', /json/)
-						.expect(200)
-					  .expect(function(res) {
-								expect(res.body.version, 'body.version').to.exist;
-						})
-			.expect(200, done);
-	});
-
+    it('displays the app version number', done => {
+        request(app)
+            .get('/status')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(res => expect(res.body.version, 'body.version').to.exist)
+            .expect(200, done);
+    });
 });
-
