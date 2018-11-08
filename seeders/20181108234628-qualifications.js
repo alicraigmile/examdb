fb4059ea4f6997c32406298a0990f73380634aed
Ali Craigmile <ali@craigmile.com>
2018-11-08 23:50:26 +0000

    Adding some default data

---
+++ seeders/20181108234628-qualifications.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const models = require('../models');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const date =  Date();
+    return queryInterface.bulkInsert('ExamBoards', [
+      { name: 'AQA', createdAt: date, updatedAt: date },
+      { name: 'EDEXCEL', createdAt: date, updatedAt: date },
+      { name: 'EDUQAS', createdAt: date, updatedAt: date },
+      { name: 'CEA', createdAt: date, updatedAt: date },
+      { name: 'OCR', createdAt: date, updatedAt: date },
+      { name: 'WJEC', createdAt: date, updatedAt: date },
+      { name: 'SQA', createdAt: date, updatedAt: date }
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('ExamBoards', {[Op.or]: [
+        { name: 'AQA' },
+        { name: 'EDEXCEL' },
+        { name: 'EDUQAS' },
+        { name: 'CEA' },
+        { name: 'OCR' },
+        { name: 'WJEC' },
+        { name: 'SQA' }
+      ]});
+  }
+};
