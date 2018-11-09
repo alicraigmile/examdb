d902258c1033a22d01d1a26a614d7712cb30aa27
Ali Craigmile <ali.craigmile@bbc.co.uk>
2018-11-09 11:07:30 +0000

    refactor(move to sequelize models)

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ import models from './models';
 import npmPackage from './package';
 import http from 'http';
 import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
       
 const port = process.env.PORT || '5000';
 app.set('port', port);
 
-const logger = morgan('combined');
-app.use(logger);
+// log only 4xx and 5xx responses to console
+app.use(morgan('dev', {
+  skip: function (req, res) { return res.statusCode < 400 }
+}))
+
+// log all requests to access.log
+app.use(morgan('common', {
+  stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+}))
 
 const server = http.createServer(app);
 
