efbe60873763bcf58358e1ed1752875b5b86f919
Ali Craigmile <ali@craigmile.com>
2018-11-17 00:22:16 +0000

    feature(json came round for tea)

--- src/routes/teapot.js
+++ src/routes/teapot.js
@@ -1,11 +1,7 @@
 import { Router } from 'express';
 
-const router = Router({ mergeParams: true }).get('/teapot', async (req, res, next) => {
-    try {
-        return res.status(418).send("418 I'm a teapot");
-    } catch (error) {
-        return next(error);
-    }
-});
+const router = Router({ mergeParams: true })
+    .get('/teapot.json', async (req, res) => res.status(418).json({ status: 418, message: "I'm a teapot" }))
+    .get('/teapot', async (req, res) => res.status(418).send("418 I'm a teapot"));
 
 export default router;
