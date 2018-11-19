ea4335a5716e9d0dd65920669c12e1196a41cd7b
Ali Craigmile <ali@craigmile.com>
2018-11-19 00:37:12 +0000

    refactor(db calls)

--- src/routes/programmesofstudy.js
+++ src/routes/programmesofstudy.js
@@ -14,15 +14,16 @@ const router = Router({ mergeParams: true })
     .get('/programmesofstudy', (req, res) => res.redirect('/'))
 
     .get('/programmesofstudy/:programmeOfStudy.json', async (req, res) => {
+        const { Course, Qualification, ProgrammeOfStudy } = req.db;
         const programmeOfStudyId = req.params.programmeOfStudy;
         try {
-            req.db.ProgrammeOfStudy.findByPk(programmeOfStudyId, {
-                include: [{ model: req.db.Qualification }]
+            ProgrammeOfStudy.findByPk(programmeOfStudyId, {
+                include: [{ model: Qualification }]
             }).then(programmeOfStudy => {
                 if (programmeOfStudy) {
                     programmeOfStudy.getCourses().then(courses => {
                         const exams = _.map(courses, async course => {
-                            await req.db.Course.build(course).getExams();
+                            await Course.build(course).getExams();
                         }).flatten();
                         const output = { programmeOfStudy, courses, exams };
                         res.json(output);
@@ -37,16 +38,17 @@ const router = Router({ mergeParams: true })
     })
 
     .get('/programmesofstudy/:programmeOfStudy.csv', async (req, res) => {
+        const { Course, Qualification, ProgrammeOfStudy } = req.db;
         const programmeOfStudyId = req.params.programmeOfStudy;
         try {
-            req.db.ProgrammeOfStudy.findByPk(programmeOfStudyId, {
-                include: [{ model: req.db.Qualification }]
+            ProgrammeOfStudy.findByPk(programmeOfStudyId, {
+                include: [{ model: Qualification }]
             }).then(programmeOfStudy => {
                 if (programmeOfStudy) {
                     programmeOfStudy.getCourses().then(courses => {
                         const exams = _.map(courses, async course => {
                             // this may be broken
-                            await req.db.Course.build(course).getExams();
+                            await Course.build(course).getExams();
                         }).flatten();
                         return res.csv(exams, true);
                     });
@@ -60,16 +62,17 @@ const router = Router({ mergeParams: true })
     })
 
     .get('/programmesofstudy/:programmeOfStudy', (req, res) => {
+        const { Course, Qualification, ProgrammeOfStudy } = req.db;
         const programmeOfStudyId = req.params.programmeOfStudy;
         try {
-            req.db.ProgrammeOfStudy.findByPk(programmeOfStudyId, {
-                include: [{ model: req.db.Qualification }]
+            ProgrammeOfStudy.findByPk(programmeOfStudyId, {
+                include: [{ model: Qualification }]
             }).then(programmeOfStudy => {
                 if (programmeOfStudy) {
                     programmeOfStudy.getCourses().then(courses => {
                         const exams = _.map(
                             courses,
-                            async course => req.db.Course.build(course).getExams() // await
+                            async course => Course.build(course).getExams() // await
                         ).flatten();
                         const output = { programmeOfStudy, courses, exams };
                         return res.render('programmeofstudy', output);
@@ -83,68 +86,4 @@ const router = Router({ mergeParams: true })
         }
     });
 
-/*
-    .get('/programmesofstudy/:programmeofstudy.json', (req, res) => {
-        const programmeOfStudyId = req.params.programmeofstudy;
-        const programmeOfStudy = req.store.programmeOfStudy(programmeOfStudyId);
-
-        if (!programmeOfStudy) return res.error.json(404, `Programme of study '${programmeOfStudyId}' was not found.`);
-
-        const exams = req.store.examsForProgrammeOfStudy(programmeOfStudy.id);
-        _.map(exams, exam => {
-            const course = req.store.course(exam.course);
-            const provider = req.store.provider(course.provider);
-            const examboard = req.store.examboard(provider.examboard);
-            exam.c = course;
-            exam.p = provider;
-            exam.e = examboard;
-            return exam;
-        });
-
-        return res.json({ programmeOfStudy, exams });
-    })
-
-    .get('/programmesofstudy/:programmeofstudy.csv', (req, res) => {
-        const programmeofstudyId = req.params.programmeofstudy;
-        const programmeofstudy = req.store.programmeOfStudy(programmeofstudyId);
-
-        if (!programmeofstudy) return res.error.text(404, `Programme of study '${programmeofstudyId}' was not found.`);
-
-        const exams = req.store.examsForProgrammeOfStudy(programmeofstudy.id);
-        return res.csv(exams, true);
-    })
-
-    .get('/programmesofstudy/:programmeofstudy', (req, res) => {
-        const programmeOfStudyId = req.params.programmeofstudy;
-        const programmeOfStudy = req.store.programmeOfStudy(programmeOfStudyId);
-
-        if (!programmeOfStudy) return res.error.html(404, `Programme of study '${programmeOfStudyId}' was not found.`);
-
-        const courses = req.store.coursesByProgrammeOfStudy(programmeOfStudy.id);
-        const qualification = req.store.qualification(programmeOfStudy.qualification);
-
-        _.map(courses, course => {
-            const provider = req.store.provider(course.provider);
-            const examboard = req.store.examboard(provider.examboard);
-            course.p = provider;
-            course.e = examboard;
-            return course;
-        });
-
-        const exams = req.store.examsForProgrammeOfStudy(programmeOfStudy.id);
-
-        _.map(exams, exam => {
-            const course = req.store.course(exam.course);
-            const provider = req.store.provider(course.provider);
-            const examboard = req.store.examboard(provider.examboard);
-            exam.c = course;
-            exam.p = provider;
-            exam.e = examboard;
-            return exam;
-        });
-
-        return res.render('programmeofstudy', { programmeOfStudy, qualification, courses, exams });
-    });
-
-    */
 export default router;
